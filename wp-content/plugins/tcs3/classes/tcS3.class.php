a370fe8cb4d754b9437f85003a23670dde7ce990
TC McCarthy <tc.mccarthy1@gmail.com>
2016-09-20 18:36:47 -0400

    Adds error logging for files that don't exist

--- wp-content/plugins/tcs3/classes/tcS3.class.php
+++ wp-content/plugins/tcs3/classes/tcS3.class.php
@@ -78,8 +78,8 @@ class tcS3 {
 		}
 
 		foreach ($keys as $key) {
-			error_log(json_encode($keys));
-			$localFile = $localdir . "/" . $key;
+			preg_match("/^(?:[0-9]+\/)?([0-9]+\/[0-9]+\/[A-Za-z0-9-_.]+)$/", $key, $matches);
+			$localFile = $localdir . "/" . $matches[1];
 			$remoteFile = $this->sanitize_s3_path($this->options["bucket_path"] . $remotedir . "/" . $key);
 
 			//error_log("Local: ".$localFile);
