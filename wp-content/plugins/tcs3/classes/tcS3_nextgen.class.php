61bb5389ab730b85ac15f358f36d3ade54398843
TC McCarthy <tc.mccarthy1@gmail.com>
2016-09-21 11:31:06 -0400

    Updates in-person-course and tcS3

--- wp-content/plugins/tcs3/classes/tcS3_nextgen.class.php
+++ wp-content/plugins/tcs3/classes/tcS3_nextgen.class.php
@@ -5,9 +5,6 @@ class tcS3_Nextgen extends tcS3{
 	public function __construct(){
 		parent::__construct();
 
-		//upload directory handling
-		$this->galleryDir = "/gallery";
-
 		if($this->use_S3){
 			$this->nextgen_register_hooks();
 		}
@@ -21,7 +18,24 @@ class tcS3_Nextgen extends tcS3{
 	}
 
 	public function nextgen_modify_url($retval, $image, $size){
-		$retval = $this->build_attachment_url($retval, $this->galleryDir);
+		error_log("========== RETVAL ================");
+		error_log(var_export($retval, true));
+		error_log("========== !RETVAL ================");
+		
+		preg_match("/wp-content\/(.+)\/[^.]+\.[A-Za-z]+$/", $retval, $matches);
+		
+		error_log("========== GalleryDir ================");
+		error_log(var_export($matches, true));
+		error_log("========== !GalleryDir ================");
+
+		$galleryDir = "/" . $matches[1];
+
+		$retval = $this->build_attachment_url($retval, $galleryDir);
+
+		error_log("========== RETVAL ================");
+		error_log(var_export($galleryDir, true));
+		error_log(var_export($retval, true));
+		error_log("========== !RETVAL ================");
 
 		//error_log($url);
 		
@@ -36,6 +50,10 @@ class tcS3_Nextgen extends tcS3{
 			WHERE pid = {$pid}
 		");
 
+		error_log("========== RETVAL ================");
+		error_log(var_export($data, true));
+		error_log("========== !RETVAL ================");
+
 		$match = "/" . preg_replace("/\//", "\/", $this->galleryDir) . "\/(.+)$/";
 
 		preg_match($match, $data->path, $matches);
@@ -43,9 +61,9 @@ class tcS3_Nextgen extends tcS3{
 		$gallery = $matches[1];
 
 		$keys = array(
-			$gallery . "/" . $data->filename,
-			$gallery . "/" . $data->filename . "_backup",
-			$gallery . "/thumbs/thumbs_" . $data->filename
+			$gallery . "/REGEXGAPFILL/" . $data->filename,
+			$gallery . "/REGEXGAPFILL/" . $data->filename . "_backup",
+			$gallery . "/REGEXGAPFILL/thumbs/thumbs_" . $data->filename
 		);
 
 		$this->delete_from_S3($keys, $this->galleryDir, false);
@@ -62,24 +80,41 @@ class tcS3_Nextgen extends tcS3{
 			AND b.gid = {$galleryid}
 		");
 
-		$match = "/" . preg_replace("/\//", "\/", $this->galleryDir) . "\/(.+)$/";
+		$galleryDir = $this->get_gallery_from_path($data->path);
+
+		$match = "/" . preg_replace("/\//", "\/", $galleryDir) . "\/(.+)$/";
 
 		preg_match($match, $data->path, $matches);
 
 		$gallery = $matches[1];
 
 		$keys = array(
-			$gallery . "/" . $data->filename,
-			$gallery . "/" . $data->filename . "_backup",
-			$gallery . "/thumbs/thumbs_" . $data->filename
+			$gallery . "/REGEXGAPFILL/" . $data->filename,
+			$gallery . "/REGEXGAPFILL/" . $data->filename . "_backup",
+			$gallery . "/REGEXGAPFILL/thumbs/thumbs_" . $data->filename
 		);
 
-		$this->push_to_s3($keys, NGG_IMPORT_ROOT . $this->galleryDir, "gallery");
+		error_log("========== Keys ================");
+		error_log(var_export($keys, true));
+		error_log(var_export(NGG_IMPORT_ROOT . $galleryDir, true));
+		error_log("========== !Keys ================");
+
+		$this->push_to_s3($keys, NGG_IMPORT_ROOT . $galleryDir, $galleryDir);
 
 	}
 
 	public function nextgen_image_updated($image) { 
 	    // make action magic happen here... 
 	    $this->dump_to_log($image);
-	} 
+	}
+
+	public function get_gallery_from_path($path){
+		preg_match("/wp-content\/(.+)\/?$/", $path, $matches);
+
+		return "/" . $matches[1];
+	}
+
+	public function get_gallery_from_url($url){
+
+	}
 }
